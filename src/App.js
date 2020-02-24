94a6521da8f2ea478b088838fb96e6b29ad00a57
Raymond To <rayto510@gmail.com>
2020-02-24 00:07:55 -0800

    Add more blocks

--- src/App.js
+++ src/App.js
@@ -23,13 +23,14 @@ function createBoard() {
 
 function App() {
   const [activeRow, setActiveRow] = useState(ROWS - 1);
-  const [activeColumn, setActiveColumn] = useState(Math.floor(Math.random() * COLUMNS));
+  const [activeColumns, setActiveColumns] = useState([0, 1, 2]);
   const [activeDirection, changeDirection] = useState(RIGHT);
+  const [activeBlocks, setActiveBlocks] = useState(3);
   const [gameOver, setGameOver] = useState(false);
   const [board, dispatch] = useReducer((board, action) => {
     switch (action.type) {
       case "update":
-        return board.map((row, index) => index === activeRow ? row.map((cell, i) => i === activeColumn ? ACTIVE : EMPTY ) : row);
+        return board.map((row, index) => index === activeRow ? row.map((cell, i) => activeColumns.includes(i) ? ACTIVE : EMPTY ) : row);
       case "reset":
         return createBoard();
       default:
@@ -43,34 +44,52 @@ function App() {
   }, !gameOver ? 125 - (ROWS - activeRow)*0.4 : null);
 
   const updatePosition = () => {
+    const leftPositions = Array.from(activeColumns, c => c - 1);
+    const rightPositions = Array.from(activeColumns, c => c + 1);
     if (activeDirection === RIGHT) {
-      if (activeColumn === COLUMNS - 1) {
+      if (activeColumns[activeColumns.length - 1] === COLUMNS - 1) {
         changeDirection(LEFT); // reached end of screen, go back
-        setActiveColumn(activeColumn - 1);
+        setActiveColumns(leftPositions);
       } else {
-        setActiveColumn(activeColumn + 1);
+        setActiveColumns(rightPositions);
       }
     } else if (activeDirection === LEFT) {
-      if (activeColumn === 0) {
+      if (activeColumns[0] === 0) {
         changeDirection(RIGHT);
-        setActiveColumn(activeColumn + 1);
+        setActiveColumns(rightPositions);
       } else {
-        setActiveColumn(activeColumn - 1);
+        setActiveColumns(leftPositions);
       }
     }
   }
 
   const userClick = () => {
     setActiveRow(activeRow - 1);
+    if (activeRow > ROWS - 2) {
+      setActiveBlocks(3);
+    } else if (activeRow > ROWS - 5) {
+      setActiveBlocks(2);
+    } else {
+      setActiveBlocks(1);
+    }
+    setActiveColumns(Array.from(activeColumns).slice(0, activeBlocks));
     if (activeRow === 0) {
       setGameOver(true);
       setActiveRow(ROWS);
+      setActiveBlocks(3);
       alert('YOU WIN!');
     }
-    if (activeRow < ROWS - 1 && board[activeRow][activeColumn] === ACTIVE && board[activeRow + 1][activeColumn] !== ACTIVE) {
-      setGameOver(true);
-      setActiveRow(ROWS);
-      alert('YOU LOSE! BETTER LUCK NEXT TIME!');
+    if (activeRow < ROWS - 1) {
+      for (let activeColumn of activeColumns) {
+        if (board[activeRow][activeColumn] === ACTIVE && board[activeRow + 1][activeColumn] !== ACTIVE) {
+          setGameOver(true);
+          setActiveRow(ROWS);
+          setActiveBlocks(3);
+          setActiveColumns([0, 1, 2]);
+          alert('YOU LOSE! BETTER LUCK NEXT TIME!');
+          break;
+        }
+      }
     }
   }
 
